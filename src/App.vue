21b5c4d45503b618e032e40bbf966facfd9ac09f
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2023-04-12 15:33:39 +0900

    名前

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,34 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import Header from './components/Header.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-    </div>
+    </div> -->
+    <Header class="header" />
   </header>
 
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
   </main>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.header {
+  margin-left: auto;
+  margin-right: auto;
 }
 
 .logo {
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="top">
+    <img src="../images/ikura-hamu_icon.PNG" class="icon">
+    <div class="name">
+      <h1>いくら・はむ</h1>
+      <h2>@ikura-hamu</h2>
+    </div>
+    
+  </div>
+</template>
+
+<style>
+.icon {
+  height: 120px;
+  width: 120px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.top{
+  display: grid;
+  grid-template-columns: 1fr 2fr; 
+}
+
+.name {
+  display: grid;
+  margin-top: auto;
+  margin-bottom: auto;
+}
+</style>
